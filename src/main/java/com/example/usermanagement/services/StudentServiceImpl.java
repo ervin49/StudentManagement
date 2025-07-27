ce237aebba8430ecef469d53a0f45d7076dbb934
ervin49 <ervinmarin49@gmail.com>
2025-07-28 00:07:43 +0300

    Modified controller methods

--- src/main/java/com/example/usermanagement/services/StudentServiceImpl.java
+++ src/main/java/com/example/usermanagement/services/StudentServiceImpl.java
@@ -1,4 +1,45 @@
 package com.example.usermanagement.services;
 
-public class StudentServiceImpl {
+import com.example.usermanagement.models.Student;
+import com.example.usermanagement.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+    StudentRepository studentRepository;
+
+    @Autowired
+    public StudentServiceImpl(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public void addStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Optional<Student> getStudentById(Integer id) {
+        return studentRepository.findById(id);
+    }
+
+    @Override
+    public void updateStudentById(Integer id, Student student) {
+        studentRepository.deleteById(id);
+        studentRepository.save(student);
+    }
+
+    @Override
+    public void deleteStudentById(Integer id) {
+        studentRepository.deleteById(id);
+    }
 }
